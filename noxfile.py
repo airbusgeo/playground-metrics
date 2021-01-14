362b27fd604644dc365beae25dd8a733d87febc3
Clément Maliet <clement.maliet@magellium.fr>
2021-01-14 14:04:49 +0100

    Fix invalid noxfile test dependencies resolve

--- noxfile.py
+++ noxfile.py
@@ -10,5 +10,5 @@ def lint(session):
 
 @nox.session(python=['3.6', '3.7', '3.8', '3.9'])
 def tests(session):
-    session.install('.[test]')
+    session.install('.[tests]')
     session.run('pytest', '-vv', '--cov-report', 'term-missing', '--cov=map_metric_api', 'tests/')
